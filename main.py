546ae42a24088c5e6b3f7e71caee8feaa5009cd9
Saurav Khanal <sauravkhanal777@gmail.com>
2025-12-06 21:05:12 +0545

    Integrate AI explanation feature

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
+from google import genai
+from dotenv import load_dotenv, dotenv_values
+import os
+
+load_dotenv()
+
+api_key = os.getenv('API_KEY')
 
 true_df= pd.read_csv("True.csv")
 fake_df= pd.read_csv("Fake.csv")
@@ -47,19 +54,29 @@ rf = RandomForestClassifier()
 rf.fit(xv_train, y_train)
 predict_rf= rf.predict(xv_test)
 
-def predict_news():
-    user_input= input("Enter the news text")
-    processed_user_input= preprocess(user_input)
-    vectorized_user_input= vectorization.transform([processed_user_input])
-    answer(lr, vectorized_user_input)
-    answer(rf, vectorized_user_input)
-
 def answer(model, vectorized_user_input):
     prediction = model.predict(vectorized_user_input)
     if prediction == 1:
-        print(f'The News is True as per {model.__class__.__name__}')
+        print(f'The News is True as per {model.__class__.__name__} \n')
     else:
         print(f'The News is Fake as per {model.__class__.__name__}')
+    return prediction[0]  
 
+def predict_news():
+    user_input = input("Enter the news text: ")
+    processed_user_input = preprocess(user_input)
+    vectorized_user_input = vectorization.transform([processed_user_input])
+
+    pred_lr = answer(lr, vectorized_user_input)
+    pred_rf = answer(rf, vectorized_user_input)
+
+    client = genai.Client(api_key=api_key)
+  
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents = f"'{user_input}' is {'true' if pred_lr == 1 else 'fake'} news according to the LogisticRegression model. Explain why in 50 words."
+    )
+    print("\nAI Explanation:")
+    print("\n AI Explanation: \n")
+    print(response.text)
 predict_news()
-
